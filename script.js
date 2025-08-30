b8a70a10fa4a6086c500500a8c4f063d40c810c3
Emerson Romana Senna <133699469+EmersonRomana@users.noreply.github.com>
2025-08-30 17:21:26 -0300

    desafio sem função

---
+++ script.js
@@ -0,0 +1,21 @@
+// ===============================
+// Textos em cada idioma
+// ===============================
+const textos = {
+  pt: {
+    h2: "Aprendizado",
+    h3: "Publicado em 21 de dezembro de 2023",
+    h1: "Fundamentos de HTML e CSS",
+    p: "These languages are the backbone of every website, defining structure, content, and presentation.",
+  },
+  en: {
+    h2: "Learning",
+    h3: "Published 21 Dec 2023",
+    h1: "HTML & CSS foundations",
+    p: "These languages are the backbone of every website, defining structure, content, and presentation.",
+  },
+};
+
+// ===============================
+// Seletores do DOM
+// ===============================
